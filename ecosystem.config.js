741d8a8b7382a11615885a828a2bed84cd815177
lavi27 <npt1237@gmail.com>
2022-10-03 02:00:56 +0900

    chore : pm2 실행파일 수정

---
+++ ecosystem.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  "name": "blog",
+  "script": "npm start",
+  "watch": false,
+  "ignore_watch": [
+    "node_modules",
+    "package.json",
+    "package-lock.json",
+    ".git"
+  ]
+}
